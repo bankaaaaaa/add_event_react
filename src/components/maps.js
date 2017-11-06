3531923a07cd7d539114950a8bcf211041327e95
sourabh kumar banka <sourabhsonu100@gmail.com>
2017-11-06 16:12:56 +0530

    initial commit

---
+++ src/components/maps.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
+import { compose, withStateHandlers } from "recompose"
+
+
+
+const { InfoBox } = require("react-google-maps/lib/components/addons/InfoBox");
+
+
+const StyledMapWithAnInfoBox = compose(
+  withStateHandlers(() => ({
+    isOpen: false,
+  }), {
+    onToggleOpen: ({ isOpen }) => () => ({
+      isOpen: !isOpen,
+    })
+  }),
+  withScriptjs,
+  withGoogleMap
+)(props =>
+  <GoogleMap
+    defaultZoom={5}
+    defaultCenter={{ lat: props.lattitude, lng: props.longitude }}
+  >
+  <Marker
+      position={{ lat: props.lattitude, lng: props.longitude }}
+    >
+      <InfoBox>
+        <div style={{ backgroundColor: `yellow`, opacity: 0.75, padding: `12px` }}>
+          <div style={{ fontSize: `16px`, fontColor: `#08233B` }}>
+            Please click to see Date<br/>
+            Event Name: {props.event}<br/>
+            lattitude:{props.lattitude},
+            longitude:{props.longitude}
+          </div>
+        </div>
+      </InfoBox>
+    </Marker>
+    
+    <Marker
+      position={{ lat: props.lattitude, lng: props.longitude }}
+      onClick={props.onToggleOpen}
+    >
+      {props.isOpen && <InfoBox
+        onCloseClick={props.onToggleOpen}
+        options={{ closeBoxURL: ``, enableEventPropagation: true }}
+      >
+        <div style={{ backgroundColor: `yellow`, opacity: 0.75, padding: `12px` }}>
+          <div style={{ fontSize: `16px`, fontColor: `#08233B` }}>
+            Date: {props.date}<br/>
+            Event Name: {props.event}<br/>
+            lattitude:{props.lattitude},
+            longitude:{props.longitude}
+          </div>
+        </div>
+      </InfoBox>}
+    </Marker>
+  </GoogleMap>
+);
+
+
+class Maps extends Component{
+  render(){
+    return(
+      <div>
+        <StyledMapWithAnInfoBox
+          googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyC4R6AN7SmujjPUIGKdyao2Kqitzr1kiRg&v=3.exp&libraries=geometry,drawing,places"
+          loadingElement={<div style={{ height: `100%` }} />}
+          containerElement={<div style={{ height: `400px` }} />}
+          mapElement={<div style={{ height: `100%` }} />}
+          lattitude={parseFloat(this.props.lattitude)}
+          longitude={parseFloat(this.props.longitude)}
+          date={this.props.date}
+          event={this.props.event}
+        />
+      </div>
+    )
+  }
+}
+export default Maps;
---
+++ src/index.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+import './index.css';
+import Maps from './components/maps'
+import Autocomplete from 'react-google-autocomplete';
+
+
+class Location extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			lattitude: '',
+			longitude: '',
+			event: '',
+			date: '',
+			clicked: false
+		};
+		this.handleInputChange = this.handleInputChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	handleInputChange(e) {
+		this.setState({
+			[e.target.name]: e.target.value
+		})
+	}
+
+	handleSubmit(e) {
+		this.setState({clicked: true});
+		e.preventDefault();
+
+	}
+	render() {
+		return(
+			<div className='container'>
+			Please enter the details:
+			<form onSubmit={this.handleSubmit} className='form-group'>
+				Lattitude: <input type="text" name="lattitude" className='form-control' onChange={this.handleInputChange} value={this.state.lattitude} required/><br />
+				Longitude: <input type="text" name="longitude" className='form-control' onChange={this.handleInputChange} value={this.state.longitude} required /><br />
+				Event: <input type="text" name="event" className='form-control' onChange={this.handleInputChange} value={this.state.event} required /><br />
+				Date: <input type='date' name="date" className='form-control' onChange={this.handleInputChange} value={this.state.date} required /><br />
+				<button type="submit" className='btn btn-lg btn-primary form-control'>Submit</button>
+			</form>
+			{this.state.clicked && <Maps lattitude={this.state.lattitude} longitude={this.state.longitude} event={this.state.event} date={this.state.date} /> }
+			<br/><br/>
+			
+			</div>
+		);
+	}
+}
+
+ReactDOM.render(
+  <Location />,
+  document.getElementById('root')
+);
