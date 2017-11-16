879b83e2d572c6ffb0f01180f1ae8e873c07ffc9
sourabh kumar banka <sourabhsonu100@gmail.com>
2017-11-16 17:12:22 +0530

    u_3

--- src/components/maps.js
+++ src/components/maps.js
@@ -64,7 +64,7 @@ class Maps extends Component{
     return(
       <div>
         <StyledMapWithAnInfoBox
-          googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyC4R6AN7SmujjPUIGKdyao2Kqitzr1kiRg&v=3.exp&libraries=geometry,drawing,places"
+          googleMapURL="https://maps.googleapis.com/maps/api/js?libraries=places&key= AIzaSyCJ3eRLjWxeiUThvW3CkHX1_S4JBfxCJ2o "
           loadingElement={<div style={{ height: `100%` }} />}
           containerElement={<div style={{ height: `400px` }} />}
           mapElement={<div style={{ height: `100%` }} />}
